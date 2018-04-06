296cfa295c70b9747f3815b67d04b2e98bfd5bde
yongming <zhuangyongming1900@gmail.com>
2018-04-06 13:57:58 +0800

    finished

--- src/decodeMessage.js
+++ src/decodeMessage.js
@@ -3,9 +3,56 @@ Each letter is paired with the letter that it coincides with when the alphabet i
 For example: "a" is encoded with "z", "b" with "y", "c" with "x", etc
 Spaces are not encoded/decoded. For example: "gsrh nvhhztv" will be decoded as "this message"
 */
+const reverseString = string =>{
+    const alphabets = [
+        "a",
+        "b",
+        "c",
+        "d",
+        "e",
+        "f",
+        "g",
+        "h",
+        "i",
+        "j",
+        "k",
+        "l",
+        "m",
+        "n",
+        "o",
+        "p",
+        "q",
+        "r",
+        "s",
+        "t",
+        "u",
+        "v",
+        "w",
+        "x",
+        "y",
+        "z"
+      ];
+      const reverseAlphabets = [...alphabets].reverse();
+      let result = [];
+      let splitstring = string.toLowerCase().split("");
+      for (let j = 0; j < splitstring.length; j++) {
+        for (let i = 0; i < alphabets.length; i++) {
+          if (splitstring[j] === alphabets[i]) {
+            result.push(reverseAlphabets[i]);
+            break;
+          }
+        }
+      }
+      return result.join("");
+}
 
-const decodeMessage = (string) => {
-
+const decodeMessage = string => {
+    let stringArr = string.split(" ");
+    let result = [];
+    for(let i=0; i<stringArr.length; i++){
+        result.push(reverseString(stringArr[i]))
+    }
+    return result.join(" ");
 };
 
 module.exports = decodeMessage;
--- test/decodeMessage.test.js
+++ test/decodeMessage.test.js
@@ -1,3 +1,4 @@
+
 // Test cases: actual --> expected
 // decodeMessage("a") --> "z"
 // decodeMessage("z") --> "a"
@@ -9,3 +10,21 @@
 
 // decodeMessage("gsrh nvhhztv") --> "this message"
 // decodeMessage("r slkv mlylwb wvxlwvh gsrh nvhhztv") --> "i hope nobody decodes this message"
+const decodeMessage = require('../src/decodeMessage')
+
+
+describe('decode of string', () => {
+    it('decode a should return z', () => {
+        expect(decodeMessage('a')).toEqual('z')
+    });
+    it('should decode sr and return hi', () => {
+        expect(decodeMessage('sr')).toEqual('hi')
+    });
+
+    it('should decode svool and return hello', () => {
+        expect(decodeMessage('svool')).toEqual('hello')
+    });
+    it("should decode r slkv mlylwb wvxlwvh gsrh nvhhztv and return i hope nobody decodes this message", ()=>{
+        expect(decodeMessage("R slkv mlylwb wvxlwvh gsrh nvhhztv")).toEqual("i hope nobody decodes this message")
+    })
+})
